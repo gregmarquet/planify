0a499a9f01573c00e1d62b1867b057fce2947b28
gregmarquet <marquet.greg@gmail.com>
2016-08-02 11:50:43 -0400

    Adds index view for stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,9 @@
 class StatsController < ApplicationController
   def index
+    @user = current_user
+    @entry = Entry.new
 
+    @category_with_most_entries = Category.with_most_entries
+    @category_with_most_completed_entries = Category.with_most_completed_entries
   end
 end
